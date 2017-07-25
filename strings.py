632945e30cf1533cc57abfebdbc9f0d837f49ad8
dan19-meet <dan19@meet.mit.edu>
2017-07-25 12:23:21 +0300

    Stopping here

---
+++ strings.py
@@ -0,0 +1,74 @@
+'''
+a = 'save '
+b = 'the '
+c = 'planet'
+
+print(a + b + c)
+'''
+
+'''
+a = 4
+b = 'panda bears'
+
+print(str(a) + " " + b)
+
+name = "Dan Buganim"
+lenName = len(name)
+
+print("Length of name is: " + str(lenName))
+
+print(name.swapcase())
+'''
+
+
+'''
+a = "MEET"
+b = "meet"
+c = "Meat"
+
+if a == b:
+    print("True")
+else:
+    print("False")
+
+if a == c:
+    print("True")
+else:
+    print("False")
+    
+if b == c:
+    print("True")
+else:
+    print("False")
+    
+if a != b:
+    print("True")
+else:
+    print("False")
+
+if a != c:
+    print("True")
+else:
+    print("False")
+
+if b != c:
+    print("True")
+else:
+    print("False")
+
+
+
+a = a.lower()
+if a == b:
+    print("True")
+else:
+    print("False")
+'''
+
+
+my_string = "bananayellowthinkhatgreyBIGcalifornia314"
+meet_value = "alexadamfarahkatamir"
+
+meet_value = my_string[12:17] + my_string[24:27]
+
+print(meet_value)
